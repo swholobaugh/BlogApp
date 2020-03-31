35f7aa3c464cfbed6433f18a2141f1cb2d3cbd9f
Rodolfo Roman <rromanv@gmail.com>
2020-03-30 22:38:35 -0500

    Cleanup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title class="font-weight-light">
+      <span>My</span>
+      <span class="font-weight-regular">Blog</span>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
